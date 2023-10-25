9efc5049ec7276f4d02b8c8031ee0baff2b2447a
charlesmoreno32 <charlesmoreno007@gmail.com>
2023-10-25 13:45:57 -0700

    started implementing post rating

--- src/api/backend.py
+++ src/api/backend.py
@@ -10,86 +10,46 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
-class Barrel(BaseModel):
-    sku: str
+class User(BaseModel):
+    user_id: int
+    name: str
+    username: str
+    email: str
+    password: str
 
-    ml_per_barrel: int
-    potion_type: list[int]
+class Shoe(BaseModel):
+    shoe_id: int
+    name: str
+    brand: str
     price: int
+    colors: str
+    material: str
+    tags: list[str]
 
-    quantity: int
+class Rating(BaseModel):
+    shoe_id: int
+    user_id: int
+    rating: int
+    comment: str
 
-@router.post("/deliver")
-def post_deliver_barrels(barrels_delivered: list[Barrel]):
+@router.post("/shoes/:shoe_id/ratings/:rating_id")
+def post_shoe_rating(Rating):
     """ """
-    gold_paid = 0
-    red_ml = 0
-    green_ml = 0
-    blue_ml = 0
-    dark_ml = 0
-    for barrel in barrels_delivered:
-        gold_paid += barrel.price * barrel.quantity
-        if(barrel.potion_type == [1,0,0,0]):
-            red_ml += barrel.ml_per_barrel * barrel.quantity
-        elif(barrel.potion_type == [0,1,0,0]):
-            green_ml += barrel.ml_per_barrel * barrel.quantity
-        elif(barrel.potion_type == [0,0,1,0]):
-            blue_ml += barrel.ml_per_barrel * barrel.quantity
-        elif(barrel.potion_type == [0,0,0,1]):
-            dark_ml += barrel.ml_per_barrel * barrel.quantity
-        else:
-            raise Exception('Invalid potion type')
-    
-    print(f"gold paid: {gold_paid} red_ml: {red_ml} blue_ml: {blue_ml} green_ml: {green_ml} dark_ml: {dark_ml}")
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text("""
-                                           INSERT INTO gold_ledger (gold_change) 
-                                           VALUES (:gold_paid)
+                                           INSERT INTO ratings (shoe_id, user_id, rating, comment) 
+                                           VALUES (:shoe_id, :user_id, :rating, :comment)
+                                           JOIN shoes AS shoes_ratings ON ratings.shoe_id = shoes.shoe_id
+                                           WHERE shoe_id = :shoe_id
                                            """),
-                                        [{"gold_paid": -gold_paid}])
-        connection.execute(sqlalchemy.text("""
-                                           INSERT INTO ml_ledger (red_ml_change, green_ml_change, blue_ml_change, dark_ml_change) 
-                                           VALUES (:red_ml, :green_ml, :blue_ml, :dark_ml)
-                                           """),
-                                        [{"red_ml": red_ml, "green_ml": green_ml,"blue_ml": blue_ml,"dark_ml": dark_ml}])
+                                        [{"shoe_id": Rating.shoe_id, "rating": Rating.rating, "rating": Rating.rating, "comment": Rating.comment}])
     return "OK"
 
 # Gets called once a day
 @router.post("/plan")
-def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
+def get_wholesale_purchase_plan(wholesale_catalog: list[User]):
     """ """
     print(wholesale_catalog)
   
-    with db.engine.begin() as connection:
-        gold = connection.execute(sqlalchemy.text("""
-                                                  SELECT SUM(gold_change)
-                                                  FROM gold_ledger
-                                                  """)).scalar_one()
-    curr_gold = gold
-    plan = []
-    times = 0
-    quants = {}
-    lst = wholesale_catalog
-    for barrel in wholesale_catalog:
-        quants[barrel.sku] = 0
-
-    
-    while(curr_gold > 99 and times < 10):
-        times += 1
-        for barrel in wholesale_catalog:
-            if(curr_gold >= barrel.price):
-                if('LARGE' in barrel.sku or 'SMALL' in barrel.sku or "DARK" in barrel.sku):
-                        quants[barrel.sku] += 1
-                        curr_gold -= barrel.price
-                        barrel.quantity -= 1
-    
-    for barrel in wholesale_catalog:
-        if(quants[barrel.sku] != 0):
-            plan.append(
-                {
-                    "sku": barrel.sku,
-                    "quantity": quants[barrel.sku]
-                }
-            )
     
     return plan
