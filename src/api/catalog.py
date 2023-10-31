805bd4609b89da16c3fedf3022d5d21ea01fdc8d
sgill33 <sgill33@calpoly.edu>
2023-10-30 17:27:14 -0700

    removed potion shop references

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -28,32 +28,3 @@ def get_shoe_catalog():
         )
     return ret
 
-@router.get("/catalog/", tags=["catalog"])
-def get_catalog():
-    """
-    Each unique item combination must have only a single price.
-    """
-
-    # Can return a max of 20 items.
-    catalog = []
-
-    with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("""
-                                                     SELECT sku, SUM(potion_ledger.potion_change) AS inventory, price, potion_type
-                                                     FROM potions
-                                                     JOIN potion_ledger ON potion_ledger.potion_id = potions.id
-                                                     GROUP BY potions.id
-                                                     """))
-    for row in result:
-        if(row.inventory > 0):
-            catalog.append(
-                {
-                    "sku": row.sku,
-                    "name": row.sku,
-                    "quantity": row.inventory,
-                    "price": row.price,
-                    "potion_type": row.potion_type,
-                }
-            )
-
-    return catalog
