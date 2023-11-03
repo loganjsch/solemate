c4500b0c90ce1ac5181dc7dd0e83539bed5a32fe
charlesmoreno32 <charlesmoreno007@gmail.com>
2023-11-03 11:04:44 -0700

    added add shoe to user collection and get user's ratings end points

--- src/api/user.py
+++ src/api/user.py
@@ -27,3 +27,33 @@ def create_user(name: str, username: str, email: int, password: str):
                                            """),
                                         [{"name": name, "username": username, "email": email, "password": password}])
     return "OK"
+
+@router.post("/{user_id}/shoes/{shoe_id}")
+def addTo_user_Collection(shoe_id: int, user_id: int):
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("""
+                                           INSERT INTO shoes_to_users (shoe_id, user_id) 
+                                           VALUES (:shoe_id, :user_id)
+                                           """),
+                                        [{"shoe_id": shoe_id, "user_id": user_id}])
+    return "OK"
+
+@router.get("/users/{user_id}/ratings")
+def get_users_ratings(user_id: int):
+    ratings = []
+    with db.engine.begin() as connection:
+        rating_list = connection.execute(sqlalchemy.text(
+                                                    """
+                                                    SELECT * FROM ratings
+                                                    WHERE user_id = :user_id
+                                                    """), 
+                                                    [{"user_id": user_id}])
+    for rating in rating_list:
+        ratings.append(
+                    {
+                    "shoe_id": rating.shoe_id,
+                    "rating": rating.rating,
+                    "comment": rating.comment
+                    }
+                    )
+    return ratings
