5bb5c38fdfeebf24cebe68c5915a1b276900664e
sgill33 <sgill33@calpoly.edu>
2023-11-04 18:43:29 -0700

    added shoe search function

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 import sqlalchemy
 from src import database as db
+from sqlalchemy import func,or_
+
 
 router = APIRouter()
 
@@ -9,25 +11,105 @@ router = APIRouter()
 def get_shoe_catalog():
     """ """
     with db.engine.begin() as connection:
-        catalog = connection.execute(sqlalchemy.text("""SELECT * 
+        catalog = connection.execute(sqlalchemy.text("""SELECT shoes.shoe_id,name,brand,AVG(rating) as avg
                                                      FROM shoes 
-                                                     ORDER BY RANDOM() 
+                                                     LEFT JOIN reviews ON shoes.shoe_id = reviews.shoe_id
+                                                     GROUP BY shoes.shoe_id
+                                                     ORDER BY RANDOM()
                                                      LIMIT 10"""))
     ret = []
     for shoe in catalog:
-        with db.engine.begin() as connection:
-            ave_rating = connection.execute(sqlalchemy.text(
-                                                """
-                                                SELECT AVG(rating)
-                                                FROM reviews
-                                                WHERE shoe_id = :shoe_id
-                                                """), 
-                                                [{"shoe_id": shoe.shoe_id}]).scalar_one()
         ret.append(
             {
                 "name": shoe.name,
                 "brand": shoe.brand,
-                "rating": ave_rating
+                "avg_rating": shoe.avg
             }
         )
     return ret
+
+
+
+@router.get("/search/shoes", tags=["search"])
+def search_shoes(
+    search_value: str = "",
+    search_page: str = ""
+):
+    
+    limit = 30
+    
+    if search_page == "":
+        search_page = 0
+    else:
+        search_page = int(search_page)
+
+    stmt = (
+        sqlalchemy.select(
+            db.shoes.c.shoe_id,
+            db.shoes.c.name,
+            db.shoes.c.brand,
+            db.shoes.c.price,
+            func.avg(db.reviews.c.rating).label("avg")
+        )
+        .join(db.reviews,db.reviews.c.shoe_id == db.shoes.c.shoe_id,isouter=True)
+        .group_by(db.shoes.c.shoe_id)
+        .offset(search_page)
+        .limit(limit)
+        .order_by(db.shoes.c.shoe_id)
+    )
+
+
+    counting = (
+        sqlalchemy.select(
+            sqlalchemy.func.count().label("count")
+        )
+        .select_from(db.shoes)
+    )
+
+    stmt = stmt.where(or_(db.shoes.c.name.ilike(f"%{search_value}%"),
+                          db.shoes.c.brand.ilike(f"%{search_value}%"),
+                          db.shoes.c.type.ilike(f"%{search_value}%"),
+                          db.shoes.c.color.ilike(f"%{search_value}%"),
+                          db.shoes.c.material.ilike(f"%{search_value}%"),
+                          func.array_to_string(db.shoes.c.tags,',').ilike(f"%{search_value}%")))
+    
+    counting = counting.where(or_(db.shoes.c.name.ilike(f"%{search_value}%"),
+                          db.shoes.c.brand.ilike(f"%{search_value}%"),
+                          db.shoes.c.type.ilike(f"%{search_value}%"),
+                          db.shoes.c.color.ilike(f"%{search_value}%"),
+                          db.shoes.c.material.ilike(f"%{search_value}%"),
+                          func.array_to_string(db.shoes.c.tags,',').ilike(f"%{search_value}%")))
+
+    with db.engine.connect() as conn:
+
+        result = conn.execute(stmt)
+        count = conn.execute(counting).scalar_one()
+
+        json = []
+        for row in result:
+            json.append(
+                {
+                    "shoe_id": row.shoe_id,
+                    "shoe_name": row.name,
+                    "brand": row.brand,
+                    "price": row.price,
+                    "rating": row.avg,
+                }
+            )
+
+    
+    #calculate search page here
+    prev = ""
+    next = ""
+
+    if search_page-limit >= 0:
+        prev = str(search_page-limit)
+
+    if search_page+limit < count:
+        next = str(search_page+limit)
+
+    return {
+        "previous": prev,
+        "next": next, 
+        "results": json
+    }
