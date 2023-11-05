ff3c5ec5e35659451b5fe215603df21b3b1e1edd
sgill33 <sgill33@calpoly.edu>
2023-11-04 18:43:19 -0700

    imported supabase tables as python objects

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import os
 import dotenv
+import sqlalchemy
 from sqlalchemy import create_engine
 
 def database_connection_url():
@@ -8,3 +9,7 @@ def database_connection_url():
     return os.environ.get("POSTGRES_URI")
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
+metadata_obj = sqlalchemy.MetaData()
+shoes = sqlalchemy.Table("shoes", metadata_obj, autoload_with = engine)
+users = sqlalchemy.Table("users", metadata_obj, autoload_with = engine)
+reviews = sqlalchemy.Table("reviews", metadata_obj, autoload_with = engine)
