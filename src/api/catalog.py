14264a8b11ca53bf876734d0ea4647a28dcbde7a
charlesmoreno32 <charlesmoreno007@gmail.com>
2023-11-27 16:22:20 -0800

    styling fixes

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from src.api import auth
 import sqlalchemy
 from src import database as db
 from sqlalchemy import func,or_
 
 
-router = APIRouter()
+router = APIRouter(
+    dependencies=[Depends(auth.get_api_key)]
+)
 
 
 @router.get("/shoes", tags=["shoes"])
