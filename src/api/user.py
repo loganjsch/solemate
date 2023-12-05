9c92a5a412f4e1f560d337f6d2db725cc653eb14
loganjsch <loganjsch@gmail.com>
2023-12-05 15:37:32 -0800

    Update user.py

--- src/api/user.py
+++ src/api/user.py
@@ -146,6 +146,17 @@ def login(username: str, password: str):
 @router.post("/{user_id}/logout")
 def logout(user_id:int):
     with db.engine.begin() as connection:
+        
+        # Check if the user exists
+        user_exists = connection.execute(sqlalchemy.text("""
+            SELECT COUNT(*) 
+            FROM users
+            WHERE user_id = :user_id
+        """), {"user_id": user_id}).scalar_one()
+
+        if user_exists == 0:
+            raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
+
         response = connection.execute(sqlalchemy.text("""
                                             SELECT is_logged_in
                                             FROM users
@@ -154,20 +165,16 @@ def logout(user_id:int):
                                         [{"user_id": user_id}]).scalar_one()
         
         if response != True:
-            return "Cannot Logout. You are not logged in."
+            return "Cannot Logout."
         
-        try:
-            # Update the user's login status if the user exists
-            connection.execute(sqlalchemy.text("""
-                UPDATE users
-                SET is_logged_in = FALSE
-                WHERE user_id = :user_id
-            """), {"user_id": user_id})
-        except Exception as e:
-            # Log the exception or handle it based on your requirements
-            raise HTTPException(status_code=404, detail="Error. Double check if user exists.")
+        connection.execute(sqlalchemy.text("""
+                                            UPDATE users
+                                            SET is_logged_in = FALSE
+                                            WHERE user_id = :user_id
+                                           """),
+                                        [{"user_id": user_id}])
 
-    return "Logged Out"
+        return "Logged Out"
         
 @router.post("/{user_id}/delete")
 def delete(user_id: str):
@@ -192,15 +199,28 @@ def delete(user_id: str):
 @router.post("/{user_id}/shoes/{shoe_id}")
 def add_shoe_to_Collection(shoe_id: int, user_id: int):
     with db.engine.begin() as connection:
-        response = connection.execute(sqlalchemy.text("""
+        is_logged_in = connection.execute(sqlalchemy.text("""
                                             SELECT is_logged_in
                                             FROM users
                                             WHERE user_id = :user_id
                                            """),
                                         [{"user_id": user_id}]).scalar_one()
         
-        if response != True:
+        if is_logged_in != True:
             return "Login to Access this feature"
+
+        # Check if both user and shoe exist
+        user_and_shoe_exist = connection.execute(sqlalchemy.text("""
+            SELECT 
+                (SELECT COUNT(*) FROM users WHERE user_id = :user_id) AS user_count,
+                (SELECT COUNT(*) FROM shoes WHERE shoe_id = :shoe_id) AS shoe_count
+        """), {"user_id": user_id, "shoe_id": shoe_id}).first()
+
+        if user_and_shoe_exist.user_count == 0:
+            raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
+
+        if user_and_shoe_exist.shoe_count == 0:
+            raise HTTPException(status_code=404, detail=f"Shoe with ID {shoe_id} not found")
 
 
         connection.execute(sqlalchemy.text("""
@@ -213,6 +233,16 @@ def add_shoe_to_Collection(shoe_id: int, user_id: int):
 @router.get("/{user_id}/reviews")
 def get_user_reviews(user_id: int):
     with db.engine.begin() as connection:
+        # Check if the user exists
+        user_exists = connection.execute(sqlalchemy.text("""
+            SELECT COUNT(*) 
+            FROM users
+            WHERE user_id = :user_id
+        """), {"user_id": user_id}).scalar_one()
+
+        if user_exists == 0:
+            raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
+
         reviews = connection.execute(sqlalchemy.text("""
                                                     SELECT shoe.name, rating, comment FROM reviews AS rating
                                                     JOIN shoes AS shoe ON shoe.shoe_id = rating.shoe_id
@@ -235,6 +265,16 @@ def get_user_reviews(user_id: int):
 def get_user_collection(user_id: int):
     """ """
     with db.engine.begin() as connection:
+        # Check if the user exists
+        user_exists = connection.execute(sqlalchemy.text("""
+            SELECT COUNT(*) 
+            FROM users
+            WHERE user_id = :user_id
+        """), {"user_id": user_id}).scalar_one()
+
+        if user_exists == 0:
+            raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
+
         collection = connection.execute(sqlalchemy.text(
                                                     """
                                                     SELECT shoes.shoe_id, name, brand, color, material, price FROM shoes
@@ -328,6 +368,16 @@ def get_orders(user_id: int):
     """ """
     with db.engine.begin() as connection:
 
+        # Check if the user exists
+        user_exists = connection.execute(sqlalchemy.text("""
+            SELECT COUNT(*) 
+            FROM users
+            WHERE user_id = :user_id
+        """), {"user_id": user_id}).scalar_one()
+
+        if user_exists == 0:
+            raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
+
         ###Authentication
         response = connection.execute(sqlalchemy.text("""
                                             SELECT is_logged_in
@@ -368,6 +418,16 @@ def get_points(user_id: int):
     """Retrieve total points for a user"""
     
     with db.engine.begin() as connection:
+
+        # Check if the user exists
+        user_exists = connection.execute(sqlalchemy.text("""
+            SELECT COUNT(*) 
+            FROM users
+            WHERE user_id = :user_id
+        """), {"user_id": user_id}).scalar_one()
+
+        if user_exists == 0:
+            raise HTTPException(status_code=404, detail=f"User with ID {user_id} not found")
 
         ###Authentication
         response = connection.execute(sqlalchemy.text("""
